a341aed1ac4fd4847d9b60075b68505421c28a46
komoliddin-a <komoliddin.asomiddinov.88@mail.ru>
2022-11-18 20:10:26 +0300

    Proyekt Qo`shilmoqda

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Card from './components/Card';
+
+function App() {
+ 
+  const [state, setState] = React.useState([]);
+
+  async function getMonsters (){
+     try{
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setState(res.data);
+     } catch(error){
+      console.log(error);
+     }
+
+  }
+
+  React.useEffect(()=>{
+    getMonsters();
+  }, [])
+
+
+  return (
+    <div className='container'>
+            {
+               state.map((monster)=> <Card key={monster.id} monster={monster}/>)
+            }
+
+    </div>
+  );
+ 
+}
+
+
+
+export default App;
